beb2921f1267b10dd67ca165bf19ec7495995dd7
Alison Hill <apreshill@gmail.com>
2021-01-02 21:07:41 -0500

    add main.js to initialize headroom

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// first import.
+import Headroom from "headroom.js";
+
+// grab the header element.
+const Header = document.querySelector("header");
+
+// construct an instance of Headroom, passing the header elemen.
+const headroom = new Headroom(Header, {
+  offset: 0,
+  tolerance: {
+    up: 0,
+    down: 0
+  },
+  classes: {
+    initial: "header--fixed",
+    pinned: "slideDown",
+    unpinned: "slideUp",
+    top: "top",
+    notTop: "not-top"
+  }
+});
+
+// initialise
+headroom.init();
+
+// When the page is at the top, remove the slideDown class.
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset === 0) {
+    Header.classList.remove("slideDown");
+  }
+});
