db410eb20ce4bc0892aa51dda3cfeba44b63c034
Alison Hill <apreshill@gmail.com>
2021-01-03 06:02:26 -0500

    kitchen sink

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,9 @@
-// first import.
+// if you're using a bundler, first import:
 import Headroom from "headroom.js";
-
-// grab the header element.
-const Header = document.querySelector("header");
-
-// construct an instance of Headroom, passing the header elemen.
-const headroom = new Headroom(Header, {
-  offset: 0,
-  tolerance: {
-    up: 0,
-    down: 0
-  },
-  classes: {
-    initial: "header--fixed",
-    pinned: "slideDown",
-    unpinned: "slideUp",
-    top: "top",
-    notTop: "not-top"
-  }
-});
-
+// grab an element
+var myElement = document.querySelector("#header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
 // initialise
 headroom.init();
 
-// When the page is at the top, remove the slideDown class.
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset === 0) {
-    Header.classList.remove("slideDown");
-  }
-});
